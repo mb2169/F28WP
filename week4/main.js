89de832f57ecda551464ff04fd7c3746e1e54336
mb2169 <mb2169@hw.ac.uk>
2023-11-03 09:51:30 +0400

    added json file

---
+++ week4/main.js
@@ -0,0 +1,20 @@
+var myCities = [
+    {
+        "name":"Dubai",
+        "country":"UAE",
+        "place":"Dubai Mall"
+    },
+    {
+        "name":"Milan",
+        "country":"Italy",
+        "place":"Duomo Di Milano"
+    },
+    {
+        "name":"Abu Dhabi",
+        "country":"UAE",
+        "place":"Marina Mall"
+    }
+];
+
+console.log(myCities[1].country);
+    
